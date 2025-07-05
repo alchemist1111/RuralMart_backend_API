e2581ac4a7adad0e936e0d4a2ee80fa9d30e450b
alchemist1111 <emoyoeugine@gmail.com>
2025-07-05 04:26:26 +0300

    updated signals file

--- users/signals.py
+++ users/signals.py
@@ -1,6 +1,4 @@
-# users/signals.py
-
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_delete
 from django.dispatch import receiver
 from .models import CustomUser, UserProfile
 
@@ -10,13 +8,8 @@ def create_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
 
-# Signal to save the user profile when the CustomUser is saved
-@receiver(post_save, sender=CustomUser)
-def save_user_profile(sender, instance, **kwargs):
-    instance.userprofile.save()
-
 # Signal to delete the user profile when the CustomUser is deleted
-@receiver(post_save, sender=CustomUser)
+@receiver(pre_delete, sender=CustomUser)
 def delete_user_profile(sender, instance, **kwargs):
     try:
         instance.userprofile.delete()
