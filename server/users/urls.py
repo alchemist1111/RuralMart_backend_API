77093f108e9cc59af06e2e039a61bf4372b3dae9
alchemist1111 <emoyoeugine@gmail.com>
2025-07-12 07:19:04 +0300

    Initial configuration

---
+++ server/users/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Add your user-related views here, like:
+    # path('register/', views.register, name='register'),
+    # path('login/', views.login, name='login'),
+]
