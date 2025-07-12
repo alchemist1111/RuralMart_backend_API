a4811df35e712f60efe95e7b2396158b5265e35c
alchemist1111 <emoyoeugine@gmail.com>
2025-07-12 07:24:02 +0300

    added login,register and logout urls

--- server/users/urls.py
+++ server/users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Add your user-related views here, like:
-    # path('register/', views.register, name='register'),
-    # path('login/', views.login, name='login'),
+   path("register/", views.register.as_view(), name="register"),
+   path("login/", views.login.as_view(), name="login"),
+    path("logout/", views.logout.as_view(), name="logout"),
 ]
