a8a649f9cae14d43bfde2d25f00a220aabc9094d
alchemist1111 <emoyoeugine@gmail.com>
2025-07-12 08:42:34 +0300

    created superuser

--- server/users/urls.py
+++ server/users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path("register/", views.register.as_view(), name="register"),
-   path("login/", views.login.as_view(), name="login"),
-    path("logout/", views.logout.as_view(), name="logout"),
+#    path("register/", views.register.as_view(), name="register"),
+#    path("login/", views.login.as_view(), name="login"),
+#     path("logout/", views.logout.as_view(), name="logout"),
 ]
