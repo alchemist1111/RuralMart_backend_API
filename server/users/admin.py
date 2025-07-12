327f4a7ee3ced6e367428a65a7a72c3a54b42f0e
alchemist1111 <emoyoeugine@gmail.com>
2025-07-12 08:39:16 +0300

    updated admin file

--- server/users/admin.py
+++ server/users/admin.py
@@ -4,16 +4,20 @@ from .models import CustomUser
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    list_display = ['email', 'first_name', 'last_name', 'phone_number', 'address', 'is_staff']
+    list_display = ['email', 'first_name', 'last_name', 'phone_number', 'address', 'is_staff', 'is_active']
     list_filter = ['is_staff', 'is_active']
     search_fields = ['email']
     ordering = ['email']
+    
+    # Fieldsets for detailed user information
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        ('Personal info', {'fields': ('first_name', 'last_name', 'phone_number', 'address')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name', 'phone_number', 'address')}),
         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+        ('Important Dates', {'fields': ('last_login', 'date_joined')}),
     )
+    
+    # Add Fieldsets for user creation
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
@@ -21,4 +25,5 @@ class CustomUserAdmin(UserAdmin):
         ),
     )
 
+# Register the custom user model with the customized UserAdmin
 admin.site.register(CustomUser, CustomUserAdmin)
