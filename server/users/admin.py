78ee89b3bd0578149c7b0fe338be8fbab79691ba
alchemist1111 <emoyoeugine@gmail.com>
2025-07-12 08:15:58 +0300

    registered custom user model in the admin file

--- server/users/admin.py
+++ server/users/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['email', 'first_name', 'last_name', 'phone_number', 'address', 'is_staff']
+    list_filter = ['is_staff', 'is_active']
+    search_fields = ['email']
+    ordering = ['email']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'phone_number', 'address')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'first_name', 'last_name', 'phone_number', 'address', 'is_active', 'is_staff', 'is_superuser')}
+        ),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
