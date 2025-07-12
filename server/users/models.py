e68b62c1d339c250481465e64f4c42e6fe6bae32
alchemist1111 <emoyoeugine@gmail.com>
2025-07-12 08:35:55 +0300

    added custom user manager

--- server/users/models.py
+++ server/users/models.py
@@ -1,5 +1,29 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+# Custom manager for the user model
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, first_name, last_name, password=None, **extra_fields):
+        """
+        Create and return a regular user with an email and password.
+        """
+        if not email:
+            raise ValueError("The Email field must be set")
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, first_name=first_name, last_name=last_name, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, first_name, last_name, password=None, **extra_fields):
+        """
+        Create and return a superuser with an email, first name, last name, and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(email, first_name, last_name, password, **extra_fields)
 
 # Custom user model inheriting from AbstractUser
 class CustomUser(AbstractUser):
@@ -18,5 +42,7 @@ class CustomUser(AbstractUser):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name', 'last_name']  # Required fields for user creation
     
+    # Link the custom manager
+    objects = CustomUserManager()
     def __str__(self):
         return f"{self.first_name} {self.last_name} <{self.email}>"
